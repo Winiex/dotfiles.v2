b84d595134bdac4869a758d7042a8be4a1d66613
winiex <winiex.nie@gmail.com>
2015-11-23 23:33:08 +0800

    Fix permission error.

--- osx/pkgs/virtualenv.bash
+++ osx/pkgs/virtualenv.bash
@@ -1,6 +1,6 @@
 if ! [[ -x $(which virtualenvwrapper.sh) ]]
 then
     clr_green "Now install virtualenvwrapper."
-    pip install virtualenvwrapper
+    sudo pip install virtualenvwrapper
     clr_green "Finished."
 fi
