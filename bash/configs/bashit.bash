76b90e8cd3a69ae53a0382f6599f34a194fd8c13
winiex <winiex.nie@gmail.com>
2016-08-16 15:09:51 +0800

    Change bash-it theme.

--- bash/configs/bashit.bash
+++ bash/configs/bashit.bash
@@ -2,7 +2,7 @@
 export BASH_IT=$GLB_USER_HOME/.dotfiles/bash/bash-it
 
 # Lock and Load a custom theme file
-export BASH_IT_THEME='zork'
+export BASH_IT_THEME='sexy'
 
 # Load bash it
 source $BASH_IT/bash_it.sh
