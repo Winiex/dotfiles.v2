8acd5b3b0e6d9a7a647bd8d052219ab49d7d63eb
winiex <winiex.nie@gmail.com>
2017-06-02 09:55:40 +0800

    Improve last-cmd alias.

--- bash/alias/tools.bash
+++ bash/alias/tools.bash
@@ -1,6 +1,6 @@
 alias save-clipboard="pbpaste | cat > ~/Desktop/clipboard.txt"
 alias load-clipboard="cat ~/Desktop/clipboard.txt | pbcopy"
-alias last-cmd="fc -ln -1 | xargs | pbcopy"
+alias last-cmd="fc -ln -1 | xargs echo | pbcopy"
+alias exec-last-cmd='bash -c "$(fc -ln -1)"'
 alias lbys="ls -alhS"
 alias lbyt="ls -alht"
-alias run-last-cmd='bash -c "$(fc -ln -1)" | pbcopy'
