e95da2702881bb3cbcddea08d644bd47332724ef
winiex <winiex.nie@gmail.com>
2017-12-10 23:49:27 +0800

    Remove fasd Bash-It plugin.

--- bash/install_plugins.bash
+++ bash/install_plugins.bash
@@ -1,4 +1,4 @@
-general_plugins=(alias-completion.plugin base.plugin.bash battery.plugin.bash browser.plugin.bash dirs.plugin.bash explain.plugin.bash extract.plugin.bash fasd.plugin.bash fzf.plugin.bash git.plugin.bash history.plugin.bash jekyll.plugin.bash jenv.plugin.bash less-pretty-cat.plugin nginx.plugin.bash projects.plugin.bash)
+general_plugins=(alias-completion.plugin base.plugin.bash battery.plugin.bash browser.plugin.bash dirs.plugin.bash explain.plugin.bash extract.plugin.bash fzf.plugin.bash git.plugin.bash history.plugin.bash jekyll.plugin.bash jenv.plugin.bash less-pretty-cat.plugin nginx.plugin.bash projects.plugin.bash)
 osx_plugins=(osx-timemachine.plugin osx.plugin.bash)
 ubuntu_plugins=()
 arch_plugins=()
